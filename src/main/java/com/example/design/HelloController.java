c0387fcbc26412b13cd5e6c161008623402ca92d
chaymaeben4 <chaymaebenlagraoui47@gmail.com>
2023-12-11 10:24:26 +0100

    my commit

--- src/main/java/com/example/design/HelloController.java
+++ src/main/java/com/example/design/HelloController.java
@@ -2,6 +2,7 @@ package com.example.design;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -17,11 +18,23 @@ public class HelloController {
     private Button connect;
     private Button Assister;
     private TextArea text ;
+    @FXML
     private TextField search;
 
 
     @FXML
-    private void onAssisterButtonClick() {
+    private void ChangerDePage() throws IOException {
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("connect.fxml"));
+        Parent page2 = loader.load();
+
+        // Créer une nouvelle scène
+        Scene scene2 = new Scene(connect);
+
+        // Obtenir la scène actuelle à partir du bouton
+        Stage stage = (Stage) connect.getScene().getWindow();
+
+        // Remplacer la scène actuelle par la nouvelle scène
+        stage.setScene(scene2);
     }
 }
