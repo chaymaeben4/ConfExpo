9eab8c5bc516cbff18e3b20e9a09d6a398427f83
chaymaeben4 <chaymaebenlagraoui47@gmail.com>
2023-12-12 15:16:35 +0100

    my commit2

--- src/main/java/com/example/design/Page1.java
+++ src/main/java/com/example/design/Page1.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Page1 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource("choix.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource("formulaire-edit.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 792);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         Image img =new Image("C:\\Users\\hp\\Desktop\\my studies\\GI2\\S1\\POO Java\\project\\design\\src\\main\\resources\\images\\logo2.png");
