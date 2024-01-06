7c8e6191a4d87a460d62fe71524d2c45af0c2e10
chaymaeben4 <chaymaebenlagraoui47@gmail.com>
2024-01-06 15:11:00 +0100

    modification

---
+++ src/main/java/com/example/design/InscriptionController.java
@@ -0,0 +1,141 @@
+package com.example.design;
+
+import Classes.Conferencier;
+import Classes.Organisateur;
+import Classes.Participant;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import module.MyJDBC;
+import java.io.IOException;
+
+public class InscriptionController {
+    @FXML
+    private TextField nom_inscription;
+    @FXML
+    private TextField prenom_inscription;
+    @FXML
+    private TextField email_inscription;
+    @FXML
+    private PasswordField mdp_inscription;
+    @FXML
+    private Button sign_up;
+    @FXML
+    private Hyperlink link;
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private void handle_sign_up() {
+        HelloController helloController = new HelloController();
+
+        String nom = nom_inscription.getText();
+        String prenom = prenom_inscription.getText();
+        String email = email_inscription.getText();
+        String motDePasse = mdp_inscription.getText();
+        // Vérifier si tous les champs sont remplis
+        if (nom.isEmpty() || prenom.isEmpty() || email.isEmpty() || motDePasse.isEmpty()) {
+            showError("Veuillez remplir tous les champs.");
+            return;
+        }
+
+        String selectedRole = ApplicationController.getInstance().getSelectedRole();
+        MyJDBC myJDBC = new MyJDBC();
+        if (email.contains("@") ) {
+        try {
+            if (myJDBC.emailExiste(email, selectedRole)) {
+                showError("L'adresse email existe déjà.");
+                return;
+            }
+                switch (selectedRole) {
+                    case "Conferencier":
+                        Conferencier conferencier = new Conferencier(nom, prenom, email, motDePasse);
+                        myJDBC.inserer_conferencier(conferencier);
+                        helloController.afficher_page(selectedRole,selectedRole,sign_up);
+                        break;
+
+                    case "Organisateur":
+                        Organisateur organisateur = new Organisateur(nom, prenom, email, motDePasse);
+                        myJDBC.inserer_organisateur(organisateur);
+                        helloController.afficher_page(selectedRole,selectedRole,sign_up);
+                        break;
+
+                    case "Participant":
+                        Participant participant = new Participant(nom, prenom, email, motDePasse);
+                        myJDBC.inserer_participant(participant);
+                        helloController.afficher_page(selectedRole,selectedRole,sign_up);
+                        break;
+
+                    default:
+                        System.out.println("Rôle non pris en charge : " + selectedRole);
+                }}
+                catch(Exception e){
+                    e.printStackTrace();
+
+            }
+        }
+        else {
+            // Afficher un message d'erreur si l'email ne contient pas "@"
+            showError("L'adresse e-mail semble incorrecte");
+        }
+    }
+    private void showError(String message) {
+        errorLabel.setText(message);
+        errorLabel.setVisible(true);
+    }
+
+//    private void afficher_page( String role) {
+//        try {
+//            // Load the FXML file for the "Connect" page
+//            FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource(role+".fxml"));
+//            Scene scene = new Scene(fxmlLoader.load(), 1100, 792);
+//            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+//            Image img = new Image("C:\\Users\\hp\\Desktop\\my studies\\GI2\\S1\\POO Java\\project\\design\\src\\main\\resources\\images\\logo2.png");
+//            Stage stage = new Stage();
+//
+//            stage.getIcons().add(img);
+//            stage.setTitle(role);
+//            stage.setScene(scene);
+//            stage.show();
+//
+//            // Close the current Stage
+//            Stage currentStage = (Stage) sign_up.getScene().getWindow();
+//            currentStage.close();
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+    @FXML
+    private void hundle_btn_inscrire() {
+        try {
+            // Load the FXML file for the "Connect" page
+            FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource("inscription.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1100, 792);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            Image img = new Image("C:\\Users\\hp\\Desktop\\my studies\\GI2\\S1\\POO Java\\project\\design\\src\\main\\resources\\images\\logo2.png");
+            Stage stage = new Stage();
+
+            stage.getIcons().add(img);
+            stage.setTitle("Inscription");
+            stage.setScene(scene);
+            stage.show();
+
+            // Close the current Stage
+            Stage currentStage = (Stage) link.getScene().getWindow();
+            currentStage.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/com/example/design/Page1.java
+++ src/main/java/com/example/design/Page1.java
@@ -15,23 +15,15 @@ public class Page1 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        //connectionDB
-//        MyJDBC conn=new MyJDBC();
-//        Conferencier conferencier=conn.getConferencierInformation();
-//        for(Session session: conferencier.getSessions()) {
-//
-//            System.out.println(session.getLieu_Session());
-//
-//        }
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource("Conferencier.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Page1.class.getResource("Bienvenue.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1100, 792);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        Image img =new Image("C:\\Users\\pc\\Documents\\s7\\Java\\MonProjet\\frontEnd\\src\\main\\resources\\images\\logo2.png");
+        Image img =new Image("C:\\Users\\hp\\Desktop\\my studies\\GI2\\S1\\POO Java\\project\\design\\src\\main\\resources\\images\\logo2.png");
         stage.getIcons().add(img);
-        stage.setTitle("Page d'Acceuil");
+        stage.setTitle("Bienvenue");
         stage.setScene(scene);
         stage.show();
     }
